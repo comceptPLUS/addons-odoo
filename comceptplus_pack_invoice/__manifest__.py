4a8539a902d0bac9a8cc9c7f1efc3f188f1c5e79
Rui Pedrosa Franco <rui.franco@gmail.com>
2018-10-15 12:32:17 +0100

    [ADD] comceptplus_pack_invoice
    Export record attachments into a compressed file

---
+++ comceptplus_pack_invoice/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Pack records (attachments) - invoices",
+    'version': '1.0',
+    'author' : 'comceptPlus',
+    'website' : 'https://comceptplus.com',
+    'category': 'Accounting',
+    'summary': "Export selected invoice's attachments into a compressed file",
+    'description': 'This module allows the user to select invoices and pack their attachments in a zip file.',
+    'depends': [
+                'account_invoicing',
+                'comceptplus_pack_records',
+                ],
+    'data': ['data/action_data.xml'],
+    'installable': True,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:

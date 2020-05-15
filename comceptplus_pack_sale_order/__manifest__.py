6b3f5a96b5728726f6164e8f80f0f523033d7daf
Rui Pedrosa Franco <rui.franco@gmail.com>
2020-05-15 08:39:23 +0100

    [UPD] comceptplus_pack_sale_order - license

--- comceptplus_pack_sale_order/__manifest__.py
+++ comceptplus_pack_sale_order/__manifest__.py
@@ -2,6 +2,7 @@
 {
     'name': "Pack records (attachments) - sale orders",
     'version': '1.0',
+    'license': 'AGPL-3',
     'author' : 'comceptPLUS',
     'website' : 'https://comceptplus.com',
     'category': 'Accounting',
